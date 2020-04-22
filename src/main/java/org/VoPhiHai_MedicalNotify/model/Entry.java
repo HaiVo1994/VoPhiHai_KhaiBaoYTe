67a50840d9b259e37dc966c3b7d1d7313140d1c9
Võ Phi Hải <vophihai1994@gmail.com>
2020-04-22 15:12:25 +0700

    Fix Model

--- src/main/java/org/VoPhiHai_MedicalNotify/model/Entry.java
+++ src/main/java/org/VoPhiHai_MedicalNotify/model/Entry.java
@@ -69,5 +69,158 @@ public class Entry {
     private String updateBy;
     private Date updateAt;
 
+    public Entry() {
+    }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Gate getGate() {
+        return gate;
+    }
+
+    public void setGate(Gate gate) {
+        this.gate = gate;
+    }
+
+    public Transport getTransport() {
+        return transport;
+    }
+
+    public void setTransport(Transport transport) {
+        this.transport = transport;
+    }
+
+    public String getSeatNo() {
+        return seatNo;
+    }
+
+    public void setSeatNo(String seatNo) {
+        this.seatNo = seatNo;
+    }
+
+    public Date getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(Date departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public Date getImmigrationDate() {
+        return immigrationDate;
+    }
+
+    public void setImmigrationDate(Date immigrationDate) {
+        this.immigrationDate = immigrationDate;
+    }
+
+    public String getPlaceTravel() {
+        return placeTravel;
+    }
+
+    public void setPlaceTravel(String placeTravel) {
+        this.placeTravel = placeTravel;
+    }
+
+    public District getProvinceDeparture() {
+        return provinceDeparture;
+    }
+
+    public void setProvinceDeparture(District provinceDeparture) {
+        this.provinceDeparture = provinceDeparture;
+    }
+
+    public District getProvinceDestination() {
+        return provinceDestination;
+    }
+
+    public void setProvinceDestination(District provinceDestination) {
+        this.provinceDestination = provinceDestination;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public Contact getContactHelpDeclare() {
+        return contactHelpDeclare;
+    }
+
+    public void setContactHelpDeclare(Contact contactHelpDeclare) {
+        this.contactHelpDeclare = contactHelpDeclare;
+    }
+
+    public List<Status> getStatuses() {
+        return statuses;
+    }
+
+    public void setStatuses(List<Status> statuses) {
+        this.statuses = statuses;
+    }
+
+    public String getListCure() {
+        return listCure;
+    }
+
+    public void setListCure(String listCure) {
+        this.listCure = listCure;
+    }
+
+    public List<HistoryOfExposure> getHistoryOfExposures() {
+        return historyOfExposures;
+    }
+
+    public void setHistoryOfExposures(List<HistoryOfExposure> historyOfExposures) {
+        this.historyOfExposures = historyOfExposures;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public boolean isDelete() {
+        return isDelete;
+    }
+
+    public void setDelete(boolean delete) {
+        isDelete = delete;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
 }
