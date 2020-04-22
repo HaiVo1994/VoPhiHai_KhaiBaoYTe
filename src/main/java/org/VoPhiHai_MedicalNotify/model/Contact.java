cd968444f384978dfe18516ef322717a2726b730
Võ Phi Hải <vophihai1994@gmail.com>
2020-04-22 15:02:54 +0700

    Fix Model

--- src/main/java/org/VoPhiHai_MedicalNotify/model/Contact.java
+++ src/main/java/org/VoPhiHai_MedicalNotify/model/Contact.java
@@ -3,6 +3,7 @@ package org.VoPhiHai_MedicalNotify.model;
 import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -26,8 +27,17 @@ public class Contact {
     @OneToMany(mappedBy = "contactHelpDeclare", fetch = FetchType.LAZY)
     private List<Entry> entries;
 
+    private Date disable_at;
+    @Column(length = 200)
+    private String disable_by;
     @ColumnDefault(value = "1")
-    private boolean isEnable;
+    private boolean isEnabled;
+    private Date create_at;
+    @Column(length = 200)
+    private String create_by;
+    private Date update_at;
+    @Column(length = 200)
+    private String update_by;
 
     public Contact() {
     }
@@ -96,11 +106,59 @@ public class Contact {
         this.entries = entries;
     }
 
-    public boolean isEnable() {
-        return isEnable;
+    public Date getDisable_at() {
+        return disable_at;
     }
 
-    public void setEnable(boolean enable) {
-        isEnable = enable;
+    public void setDisable_at(Date disable_at) {
+        this.disable_at = disable_at;
+    }
+
+    public String getDisable_by() {
+        return disable_by;
+    }
+
+    public void setDisable_by(String disable_by) {
+        this.disable_by = disable_by;
+    }
+
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        isEnabled = enabled;
+    }
+
+    public Date getCreate_at() {
+        return create_at;
+    }
+
+    public void setCreate_at(Date create_at) {
+        this.create_at = create_at;
+    }
+
+    public String getCreate_by() {
+        return create_by;
+    }
+
+    public void setCreate_by(String create_by) {
+        this.create_by = create_by;
+    }
+
+    public Date getUpdate_at() {
+        return update_at;
+    }
+
+    public void setUpdate_at(Date update_at) {
+        this.update_at = update_at;
+    }
+
+    public String getUpdate_by() {
+        return update_by;
+    }
+
+    public void setUpdate_by(String update_by) {
+        this.update_by = update_by;
     }
 }
